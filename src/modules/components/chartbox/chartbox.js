14e19530aad20e4b6f58df3178312595eec4a9f8
Dipankar Sahoo <dipankarsahoo180@gmail.com>
2024-03-20 09:39:17 +0530

    chart.js data integration

--- src/modules/components/chartbox/chartbox.js
+++ src/modules/components/chartbox/chartbox.js
@@ -1,8 +1,23 @@
-import { LightningElement } from "lwc";
+import { LightningElement,api } from "lwc";
 import Chart from 'chart.js/auto'
 export default class Chartbox extends LightningElement{
     chart
     chartLoaded = false
+    _data = [];
+    labels
+    @api 
+    get chartRecords(){}
+    set chartRecords(data){
+        this._data = [...data.results];
+        this.labels = [...data.labels];
+        if(this.chartLoaded){
+            const elem = this.template.querySelector('.chart')
+            elem.innerHTML=''
+            this.chart.destroy()
+            this.renderChart()
+        }
+    }
+
     renderedCallback(){
         if(this.chartLoaded){
             return
@@ -18,19 +33,10 @@ export default class Chartbox extends LightningElement{
         const config = {
             type: 'doughnut',
             data: {
-                labels: [
-                  'Red',
-                  'Blue',
-                  'Yellow'
-                ],
+                labels: this.labels,
                 datasets: [{
                   label: 'My First Dataset',
-                  data: [300, 50, 100],
-                  backgroundColor: [
-                    'rgb(255, 99, 132)',
-                    'rgb(54, 162, 235)',
-                    'rgb(255, 205, 86)'
-                  ],
+                  data: this._data,
                   hoverOffset: 4
                 }]
               }
