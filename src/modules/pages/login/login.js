dbdaf6592f866db93f4ea7186d7230484926868c
Dipankar Sahoo <dipankarsahoo180@gmail.com>
2024-03-21 15:32:28 +0530

    integrate local login url

--- src/modules/pages/login/login.js
+++ src/modules/pages/login/login.js
@@ -1,5 +1,8 @@
 import { LightningElement } from "lwc";
-
+const SERVER_URL = 'http://localhost:3002'
 export default class Login extends LightningElement{
 
+    get loginUrl(){
+        return `${SERVER_URL}/oauth2/login`
+    }
 }
