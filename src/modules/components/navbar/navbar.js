13eb8466d54345bcf486003345bc4a2fc752b6c0
Dipankar Sahoo <dipankarsahoo180@gmail.com>
2024-03-21 18:38:20 +0530

    get logged in user info

--- src/modules/components/navbar/navbar.js
+++ src/modules/components/navbar/navbar.js
@@ -1,3 +1,10 @@
-import { LightningElement } from "lwc";
+import { LightningElement,api } from "lwc";
 
-export default class Navbar extends LightningElement{}
+export default class Navbar extends LightningElement{
+    @api loggedInUser
+
+    get userName(){
+        return this.loggedInUser? this.loggedInUser.display_name :''
+    }
+
+}
